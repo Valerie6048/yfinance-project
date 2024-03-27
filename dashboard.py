516324eae31d876a12e2368603c7f2c310c743b6
Akhmad Nizar Zakaria <88196359+Valerie6048@users.noreply.github.com>
2024-03-27 23:08:54 +0700

    haloo

--- dashboard.py
+++ dashboard.py
@@ -34,6 +34,102 @@ In this Projec I want to make an Analysis, Visualization, Prediction, and Sentim
 stockToken = st.text_input('Insert the Stock Token', 'NVDA')
 stock = yf.Ticker(stockToken)
 
+def get_first_trading_date(symbol):
+    # Membuat objek ticker
+    ticker = yf.Ticker(symbol)
+
+    # Mendapatkan riwayat harga
+    history = ticker.history(period="max")
+
+    # Mengambil tanggal pertama saat harga tersedia
+    first_trading_date = history.index[0]
+
+    return first_trading_date
+
+def get_last_trading_date(symbol):
+    # Creating a Ticker object
+    ticker = yf.Ticker(symbol)
+
+    # Getting historical prices
+    history = ticker.history(period="max")
+
+    # Getting the last trading date
+    last_trading_date = history.index[-1]
+
+    return last_trading_date
+
+def get_company_name(symbol):
+    # Creating a Ticker object
+    ticker = yf.Ticker(symbol)
+
+    # Getting company info
+    company_info = ticker.info
+
+    # Getting company name
+    company_name = company_info['longName']
+
+    return company_name
+
+company_name = get_company_name(stock_token)
+
+first_trading_date = get_first_trading_date(stock_token)
+
+def plot_stock_data(stock_symbol):
+    # Fetch stock data
+    stock = yf.Ticker(stock_symbol)
+    company_name = stock.info['longName']
+    first_trading_date = stock.info['firstTradeDate']
+    end_date = pd.Timestamp.today(tz='America/New_York').ceil('D')
+    start_date = first_trading_date
+    data = stock.history(start=start_date, end=end_date, interval='1d').reset_index()
+
+    years_difference = (end_date - start_date).days / 365
+
+    # Create Plotly figure
+    fig = go.Figure(data=go.Scatter(
+        x=data['Date'],
+        y=data['Close'],
+        mode='lines',
+        name='Close Price'
+    ))
+    fig.update_layout(
+        title=company_name,
+        title_x=0.5,
+        autosize=False,
+        width=900,
+        height=500,
+        xaxis=dict(rangeselector=dict(
+            buttons=list([
+                dict(count=30,
+                     label="30D",
+                     step="day",
+                     stepmode="backward"),
+                dict(count=6,
+                     label="6M",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="YTD",
+                     step="year",
+                     stepmode="todate"),
+                dict(count=1,
+                     label="1Y",
+                     step="year",
+                     stepmode="backward"),
+                dict(count=3,
+                     label="3Y",
+                     step="year",
+                     stepmode="backward"),
+                dict(count=years_difference,
+                     label="MAX",
+                     step="year",
+                     stepmode="backward")
+            ])
+        )),
+    )
+    return fig
+
+
 with st.sidebar:
     st.title('Biodata')
     """
@@ -97,3 +193,9 @@ with tabs1:
 
     with right_column:
         st.markdown(markdown_text_right)
+
+with tabs2:
+    st.header("NJAY")
+
+    fig = plot_stock_data(stock)
+    st.plotly_chart(fig)
