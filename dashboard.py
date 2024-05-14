1612ebe069aa681aa0790a9e69691a9f7906ef37
Akhmad Nizar Zakaria <88196359+Valerie6048@users.noreply.github.com>
2024-05-14 23:11:23 +0700

    hehe

--- dashboard.py
+++ dashboard.py
@@ -248,115 +248,6 @@ with tabs3:
     start_date = end_date - timedelta(days=120)  # 4 months before today
     stock_data = yf.download(stockToken, start=start_date, end=end_date)
 
-    df = yf.download(tickers=stockToken,period='1d',interval='1m')
-
-    df['MA5'] = df['Close'].rolling(window=5).mean()
-    df['MA20'] = df['Close'].rolling(window=20).mean()
-
-    macd = MACD(close=df['Close'], 
-            window_slow=26,
-            window_fast=12, 
-            window_sign=9)
-
-    # stochastic
-    stoch = StochasticOscillator(high=df['High'],
-                                close=df['Close'],
-                                low=df['Low'],
-                                window=14, 
-                                smooth_window=3)
-    
-    viz = go.Figure()
-
-    viz = make_subplots(rows=4, cols=1, shared_xaxes=True,
-                    vertical_spacing=0.01, 
-                    row_heights=[0.5,0.1,0.2,0.2])
-    
-    viz.add_trace(go.Candlestick(x=df.index,
-                open=df['Open'],
-                high=df['High'],
-                low=df['Low'],
-                close=df['Close'], name = 'market data'))
-    
-    viz.add_trace(go.Scatter(x=df.index, 
-                            y=df['MA5'], 
-                            opacity=0.7, 
-                            line=dict(color='blue', width=2), 
-                            name='MA 5'))
-
-    viz.add_trace(go.Scatter(x=df.index, 
-                            y=df['MA20'], 
-                            opacity=0.7, 
-                            line=dict(color='orange', width=2), 
-                            name='MA 20'))
-
-    # Plot volume trace on 2nd row
-    colors = ['green' if row['Open'] - row['Close'] >= 0 
-            else 'red' for index, row in df.iterrows()]
-    viz.add_trace(go.Bar(x=df.index, 
-                        y=df['Volume'],
-                        marker_color=colors
-                        ), row=2, col=1)
-
-    # Plot MACD trace on 3rd row
-    colorsM = ['green' if val >= 0 
-            else 'red' for val in macd.macd_diff()]
-    viz.add_trace(go.Bar(x=df.index, 
-                        y=macd.macd_diff(),
-                        marker_color=colorsM
-                        ), row=3, col=1)
-    viz.add_trace(go.Scatter(x=df.index,
-                            y=macd.macd(),
-                            line=dict(color='black', width=2)
-                            ), row=3, col=1)
-    viz.add_trace(go.Scatter(x=df.index,
-                            y=macd.macd_signal(),
-                            line=dict(color='blue', width=1)
-                            ), row=3, col=1)
-
-    # Plot stochastics trace on 4th row
-    viz.add_trace(go.Scatter(x=df.index,
-                            y=stoch.stoch(),
-                            line=dict(color='black', width=2)
-                            ), row=4, col=1)
-    viz.add_trace(go.Scatter(x=df.index,
-                            y=stoch.stoch_signal(),
-                            line=dict(color='blue', width=1)
-                            ), row=4, col=1)
-
-    # update layout by changing the plot size, hiding legends & rangeslider, and removing gaps between dates
-    viz.update_layout(height=900, width=1200, 
-                    showlegend=False, 
-                    xaxis_rangeslider_visible=False)
-                    
-
-    # Make the title dynamic to reflect whichever stock we are analyzing
-    viz.update_layout(
-        title= str(stock)+' Live Share Price:',
-        yaxis_title='Stock Price (USD per Shares)') 
-
-    # update y-axis label
-    viz.update_yaxes(title_text="Price", row=1, col=1)
-    viz.update_yaxes(title_text="Volume", row=2, col=1)
-    viz.update_yaxes(title_text="MACD", showgrid=False, row=3, col=1)
-    viz.update_yaxes(title_text="Stoch", row=4, col=1)           
-
-    viz.update_xaxes(
-        rangeslider_visible=False,
-        rangeselector_visible=False,
-        rangeselector=dict(
-            buttons=list([
-                dict(count=15, label="15m", step="minute", stepmode="backward"),
-                dict(count=45, label="45m", step="minute", stepmode="backward"),
-                dict(count=1, label="HTD", step="hour", stepmode="todate"),
-                dict(count=3, label="3h", step="hour", stepmode="backward"),
-                dict(step="all")
-            ])
-        )
-    )
-    st.pyplot(viz)
-
-
-
     # Calculate technical indicators using pandas-ta
     stock_data.ta.macd(append=True)
     stock_data.ta.rsi(append=True)
@@ -484,3 +375,112 @@ with tabs4:
     st.write(response.text)
     
 st.caption("@Valerie6048")
+
+/*
+    df = yf.download(tickers=stockToken,period='1d',interval='1m')
+
+    df['MA5'] = df['Close'].rolling(window=5).mean()
+    df['MA20'] = df['Close'].rolling(window=20).mean()
+
+    macd = MACD(close=df['Close'], 
+            window_slow=26,
+            window_fast=12, 
+            window_sign=9)
+
+    # stochastic
+    stoch = StochasticOscillator(high=df['High'],
+                                close=df['Close'],
+                                low=df['Low'],
+                                window=14, 
+                                smooth_window=3)
+    
+    viz = go.Figure()
+
+    viz = make_subplots(rows=4, cols=1, shared_xaxes=True,
+                    vertical_spacing=0.01, 
+                    row_heights=[0.5,0.1,0.2,0.2])
+    
+    viz.add_trace(go.Candlestick(x=df.index,
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'], name = 'market data'))
+    
+    viz.add_trace(go.Scatter(x=df.index, 
+                            y=df['MA5'], 
+                            opacity=0.7, 
+                            line=dict(color='blue', width=2), 
+                            name='MA 5'))
+
+    viz.add_trace(go.Scatter(x=df.index, 
+                            y=df['MA20'], 
+                            opacity=0.7, 
+                            line=dict(color='orange', width=2), 
+                            name='MA 20'))
+
+    # Plot volume trace on 2nd row
+    colors = ['green' if row['Open'] - row['Close'] >= 0 
+            else 'red' for index, row in df.iterrows()]
+    viz.add_trace(go.Bar(x=df.index, 
+                        y=df['Volume'],
+                        marker_color=colors
+                        ), row=2, col=1)
+
+    # Plot MACD trace on 3rd row
+    colorsM = ['green' if val >= 0 
+            else 'red' for val in macd.macd_diff()]
+    viz.add_trace(go.Bar(x=df.index, 
+                        y=macd.macd_diff(),
+                        marker_color=colorsM
+                        ), row=3, col=1)
+    viz.add_trace(go.Scatter(x=df.index,
+                            y=macd.macd(),
+                            line=dict(color='black', width=2)
+                            ), row=3, col=1)
+    viz.add_trace(go.Scatter(x=df.index,
+                            y=macd.macd_signal(),
+                            line=dict(color='blue', width=1)
+                            ), row=3, col=1)
+
+    # Plot stochastics trace on 4th row
+    viz.add_trace(go.Scatter(x=df.index,
+                            y=stoch.stoch(),
+                            line=dict(color='black', width=2)
+                            ), row=4, col=1)
+    viz.add_trace(go.Scatter(x=df.index,
+                            y=stoch.stoch_signal(),
+                            line=dict(color='blue', width=1)
+                            ), row=4, col=1)
+
+    # update layout by changing the plot size, hiding legends & rangeslider, and removing gaps between dates
+    viz.update_layout(height=900, width=1200, 
+                    showlegend=False, 
+                    xaxis_rangeslider_visible=False)
+                    
+
+    # Make the title dynamic to reflect whichever stock we are analyzing
+    viz.update_layout(
+        title= str(stock)+' Live Share Price:',
+        yaxis_title='Stock Price (USD per Shares)') 
+
+    # update y-axis label
+    viz.update_yaxes(title_text="Price", row=1, col=1)
+    viz.update_yaxes(title_text="Volume", row=2, col=1)
+    viz.update_yaxes(title_text="MACD", showgrid=False, row=3, col=1)
+    viz.update_yaxes(title_text="Stoch", row=4, col=1)           
+
+    viz.update_xaxes(
+        rangeslider_visible=False,
+        rangeselector_visible=False,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=15, label="15m", step="minute", stepmode="backward"),
+                dict(count=45, label="45m", step="minute", stepmode="backward"),
+                dict(count=1, label="HTD", step="hour", stepmode="todate"),
+                dict(count=3, label="3h", step="hour", stepmode="backward"),
+                dict(step="all")
+            ])
+        )
+    )
+    st.pyplot(viz)
+*/
